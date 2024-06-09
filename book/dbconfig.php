11b3626a15001382a470b573bd8bccf9745d35b4
Devin Liang <devinliang@gmail.com>
2024-06-09 08:49:07 +0800

    Update dbconfig.php

--- book/dbconfig.php
+++ book/dbconfig.php
@@ -1,29 +1,10 @@
 <?php
 // 設定主機、資料庫名稱、權限帳密
-
-// infinityfree
-$hostname = 'sql306.infinityfree.com';
-$database = 'if0_36696672_school';
-$dbuser = 'if0_36696672';
-$dbpass = 'SAUGZDa9LHthQ2V';
-
-// byethost
-// $hostname = 'sql201.byethost18.com';
-// $database = 'b18_36689061_school';
-// $dbuser = 'b18_36689061';
-// $dbpass = 'aaty5379';
-
-// awardspace
-// $hostname = 'fdb1032.awardspace.net';
-// $database = '4493264_school';
-// $dbuser = '4493264_school';
-// $dbpass = 'aaty5379';
-
 // localhost
-// $hostname = 'localhost';
-// $database = 'school';
-// $dbuser = 'root';
-// $dbpass = '';
+$hostname = 'localhost';
+$database = 'school';
+$dbuser = 'root';
+$dbpass = '';
 
 $main = 'booklist.php';
 ?>
