22054624f213cbbe1fcaf4d61e884c21e23410f3
Devin Liang <devinliang@gmail.com>
2024-06-11 11:27:06 +0800

    update

--- book/dbconfig.php
+++ book/dbconfig.php
@@ -1,10 +1,10 @@
 <?php
 // 設定主機、資料庫名稱、權限帳密
 // localhost
-$hostname = 'localhost';
-$database = 'school';
-$dbuser = 'root';
-$dbpass = '';
+$hostname = 'sql200.byethost31.com';
+$database = 'b31_36709395_school';
+$dbuser = 'b31_36709395';
+$dbpass = 'abc123';
 
 $main = 'booklist.php';
 ?>
